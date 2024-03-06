4e960b626d5ea8144fd32135d39b6e71a39d7e83
Sharon Ouma <118636840+sharonouma@users.noreply.github.com>
2024-03-06 16:55:11 +0300

    Update urls.py

--- PesaHub/PesaConnect/urls.py
+++ PesaHub/PesaConnect/urls.py
@@ -1,3 +1,27 @@
+"""
+URL Patterns for Mamapesa-backend API
+
+These URL patterns define the endpoints for various API views.
+
+Usage:
+    These urlpatterns are used within Django to route HTTP requests to the appropriate views.
+
+Overview:
+    - /custom-users/: Endpoint for CustomUser API operations.
+    - /trust-scores/: Endpoint for TrustScore API operations.
+    - /savings/: Endpoint for Savings API operations.
+    - /loans/: Endpoint for Loan API operations.
+    - /items/: Endpoint for Item API operations.
+    - /loan-items/: Endpoint for LoanItem API operations.
+    - /transactions/: Endpoint for Transaction API operations.
+    - /savings-items/: Endpoint for SavingsItem API operations.
+    - /withdrawals/: Endpoint for Withdrawal API operations.
+    - /user-details/: Endpoint for UserDetails API operations.
+    - /savings-payments/: Endpoint for SavingsPayment API operations.
+
+
+"""
+
 from django.urls import path
 from . import views
 from . import views
