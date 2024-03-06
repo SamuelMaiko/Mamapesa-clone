b0f7cf2425d3133915cce01a02bc3b1c6769fab6
Samuel Maiko <sammymaiko721@gmail.com>
2024-03-06 18:18:49 +0300

    clear master

--- PesaHub/PesaConnect/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PesaconnectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PesaConnect'
--- savingsandloans/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LoanRequestView, LoanRepaymentView
-
-urlpatterns = [   
-    path('loans/', LoanRequestView.as_view(), name='loans'),
-    path('repayments/', LoanRepaymentView.as_view(), name='repayments'),
-]
