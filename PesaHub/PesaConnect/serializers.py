f6f5d80960d286f57e38ea0455d69aacdd81d8b3
Sharon Ouma <118636840+sharonouma@users.noreply.github.com>
2024-03-06 16:53:03 +0300

    Update serializers.py

--- PesaHub/PesaConnect/serializers.py
+++ PesaHub/PesaConnect/serializers.py
@@ -1,3 +1,30 @@
+
+"""
+Serializers for Mamapesa-backend API
+
+These serializers convert Django models into JSON format and vice versa for API endpoints.
+
+Usage:
+    These serializers are used within Django REST Framework to handle serialization and deserialization of Django model instances.
+
+Overview:
+    - CustomUserSerializer: Serializer for the CustomUser model.
+    - TrustScoreSerializer: Serializer for the TrustScore model.
+    - SavingsSerializer: Serializer for the Savings model.
+    - LoanSerializer: Serializer for the Loan model.
+    - ItemSerializer: Serializer for the Item model.
+    - LoanItemSerializer: Serializer for the LoanItem model.
+    - TransactionSerializer: Serializer for the Transaction model.
+    - SavingsItemSerializer: Serializer for the SavingsItem model.
+    - WithdrawalSerializer: Serializer for the Withdrawal model.
+    - UserDetailsSerializer: Serializer for the UserDetails model.
+    - SavingsPaymentSerializer: Serializer for the SavingsPayment model.
+
+Each serializer defines a Meta class specifying the corresponding model and the fields to be included in the serialization process.
+
+For more information on Django REST Framework serializers, refer to the official documentation: https://www.django-rest-framework.org/api-guide/serializers/
+
+"""
 from rest_framework import serializers
 from .models import CustomUser, TrustScore, Savings, Loan, Item, LoanItem, Transaction, SavingsItem, Withdrawal, UserDetails, SavingsPayment
 class CustomUserSerializer(serializers.ModelSerializer):
