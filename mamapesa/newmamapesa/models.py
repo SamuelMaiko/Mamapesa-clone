a5411d24780279aa009bdecda0069d4381b31eb5
Samuel Maiko <sammymaiko721@gmail.com>
2024-03-13 22:11:42 +0300

    password reset in progress

--- mamapesa/newmamapesa/models.py
+++ mamapesa/newmamapesa/models.py
@@ -13,6 +13,7 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser, PermissionsMixin):
     phone_number = models.CharField(max_length=10, unique=True, blank=True, null=True)
     username = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    email = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
