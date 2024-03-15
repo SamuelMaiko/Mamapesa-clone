8d7567bbf59c73d1aee349879401780eaf0baf51
username <barasamica@gmail.com>
2024-03-15 16:13:22 +0300

    cleared

--- mamapesa/newmamapesa/helper_functions.py
+++
@@ -1,2 +0,0 @@
-def get_amount_disbursed():
-        pass
--- mamapesa/savingsandloans/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from newmamapesa.models import Loan, CustomUser
-
-admin.site.register(Loan)
-# admin.site.register(CustomUser)
-# admin.site.register(LoanTransaction)
